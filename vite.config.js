478f5573f817630f575cb9dcc824ec1a062514a8
paraversal <104731964+paraversal@users.noreply.github.com>
2025-10-19 23:16:16 +0200

    try fix

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/StudIPCalendarRenamerWeb/', 
-
+build: {
+    rollupOptions: {
+      external: ['ical.js'],
+    },
+  },
 });
