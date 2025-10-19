3823d0901f4ee5e6b0d6bb2f6b369a371855f56a
paraversal <104731964+paraversal@users.noreply.github.com>
2025-10-19 23:06:32 +0200

    fix imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-  base: '/StudIPCalendarRenamerWeb/',
-  optimizeDeps: {
-    include: ['ical.js']
+  base: '/StudIPCalendarRenamerWeb/', // your repo name
+  plugins: [
+    // Add these plugins so Rollup can resolve CommonJS modules
+    nodeResolve({ browser: true }),
+    commonjs()
+  ],
+  build: {
+    rollupOptions: {
+      // ensure ical.js is bundled, not external
+      external: [],
+    }
   }
 });
