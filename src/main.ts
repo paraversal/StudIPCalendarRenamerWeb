8b83704c62030f98943c87131d36147dfd2a4fff
paraversal <104731964+paraversal@users.noreply.github.com>
2025-10-20 22:56:15 +0200

    animations + user interaction

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 // @ts-ignore
 import ICAL from "https://unpkg.com/ical.js/dist/ical.min.js";
-
 import Handlebars from "handlebars";
 import { v4 as uuidv4 } from 'uuid';
+import * as bootstrap from "bootstrap" 
 
 type DisplayEvent = {
   final_name: string;
@@ -12,45 +12,72 @@ type DisplayEvent = {
 type App = {
   ical: any;
   event_map: Map<string, DisplayEvent>;
+  previouslySelectedIdsAmount: number;
+  modalOpen: boolean;
 }
 
-var app: App = {ical: ICAL.parse(""), event_map: new Map}
+let texts = {shortText: "Rename", longText: "Merge + rename"}
+
+var app: App = {ical: ICAL.parse(""), event_map: new Map, previouslySelectedIdsAmount: 0, modalOpen: false}
+
 
 document.addEventListener("DOMContentLoaded", _ => {
   let fileInput = document.querySelector<HTMLInputElement>('#calendar-input')!
   fileInput.addEventListener('change', async (event) => {
       const selectedFile = (event.target as HTMLInputElement).files[0];
       const file_contents = await selectedFile.text()
-      handleLoadedCalendarFile(file_contents)
+      await handleLoadedCalendarFile(file_contents)
      
   });
 
+  document.addEventListener("keydown", e => {
+    if (e.key == "r" && !app.modalOpen) {
+      let modal = document.querySelector("#renameModal")!
+      bootstrap.Modal.getOrCreateInstance(modal).show()
+      app.modalOpen = true
+    }
+  })
+
+  let newNameForm = document.querySelector("#new-name-form")!
+  newNameForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await handleRenameModalConfirm()
+  })
+
   let renameModalConfirmButton = document.querySelector<HTMLButtonElement>("#btn-rename-modal-confirm")!
-  renameModalConfirmButton.addEventListener("click", handleRenameModalConfirm)
+  renameModalConfirmButton.addEventListener("click", async () =>  handleRenameModalConfirm())
 
   let saveButton = document.querySelector("#save-button")!
-  saveButton.addEventListener("click", handleSaveCalendar)
+  saveButton.addEventListener("click", async () => handleSaveCalendar())
 
   let renameModal = document.querySelector("#renameModal")!
   let renameModalInput = document.querySelector<HTMLInputElement>("#new-name-modal-input")!
 
   renameModal.addEventListener('shown.bs.modal', () => {
+    app.modalOpen = true
     renameModalInput.value = ""
     renameModalInput.focus()
 })
 
 })
 
+Handlebars.registerHelper('addOne', function(value) {
+  return value + 1;
+});
+
 let calendarTemplate = Handlebars.compile(`
   <ul class="list-group">
     {{#each items}}
-      <li class="list-group-item lecture-entry-listitem prevent-select" id={{this.id}}>
+      <li class="list-group-item lecture-entry-listitem prevent-select" 
+          id="{{this.id}}" 
+          style="--index: {{addOne @index}};">
           <input class="form-check-input me-1 disable-click prevent-select hidden" type="checkbox" value="{{this.final_name}}">
-          <label class="prevent-select"}>{{this.final_name}}</label>
+          <label class="prevent-select">{{this.final_name}}</label>
       </li>
     {{/each}}
   </ul>
-  `)
+`)
+
 
 function onlyUnique(value: any, index: any, array: any[]) {
   return array.indexOf(value) === index;
@@ -124,56 +151,108 @@ function getDisplayCalendarEvents(): DisplayEvent[] {
   return displayEvents
 }
 
-function handleLectureEntryClick(e: Event){
+async function handleLectureEntryClick(e: Event){
   handleCalendarEntryClick(e)
   let checkedIds = getCheckedIds()
-  let button = document.querySelector("#merge-button")!
+  let button = document.querySelector<HTMLButtonElement>("#merge-button")!
   let renameModalTitle = document.querySelector("#rename-modal-title")!
 
-  if (checkedIds.length == 0) {
-    if (!button.classList.contains("d-none")) {
-      button.classList.add("d-none")
-    }
-  } else if  (checkedIds.length == 1) {
-    button.classList.remove("d-none")
-    button.innerHTML = "Rename event"
-    renameModalTitle.innerHTML = "Rename event"
-  } else {
-    button.classList.remove("d-none")
-    button.innerHTML = "Merge + rename events"
-    renameModalTitle.innerHTML = "Merge + rename events"
+  let buttonShouldAppearAfterFirstSelect = checkedIds.length == 1 && app.previouslySelectedIdsAmount == 0
+  let buttonShouldRenameAfterSelectDownToOne = checkedIds.length == 1 && app.previouslySelectedIdsAmount > 1
+  let buttonShouldRenameAfterMultipleSelect = checkedIds.length > 1 && app.previouslySelectedIdsAmount == 1
+  let buttonShouldDisappear = checkedIds.length == 0
+
+  if (checkedIds.length == 0 && app.previouslySelectedIdsAmount == 0) {
+    // this should never happen
   }
+
+  if (buttonShouldDisappear) {
+    button.classList.add("fade-out")
+    let animationDone = waitForAnimationEnd(button)
+    button.style.animationPlayState = "running"
+    await Promise.all(await animationDone)
+    button.style.animationPlayState = "paused"
+    button.classList.remove("fade-out")
+    button.classList.add("d-none")
+
+  } else if (buttonShouldAppearAfterFirstSelect) {  
+    button.classList.remove("d-none")
+    button.classList.add("fade-in")
+    let animationDone = waitForAnimationEnd(button)
+    button.style.animationPlayState = "running"
+    await Promise.all(await animationDone)
+    button.style.animationPlayState = "paused"
+    button.classList.remove("fade-in")
+    
+    button.innerHTML = texts.shortText
+    button.style.width = `${button.style.width}px`
+  } else if (buttonShouldRenameAfterSelectDownToOne) {
+    button.innerHTML = texts.shortText
+    renameModalTitle.innerHTML = texts.shortText
+  } else if (buttonShouldRenameAfterMultipleSelect) {
+    button.innerHTML = texts.longText
+    renameModalTitle.innerHTML = texts.longText
+  }
+  app.previouslySelectedIdsAmount = checkedIds.length;
 }
-function populateCalendarEntries() {
+async function populateCalendarEntries() {
   let lectureList = document.querySelector("#lecture-list")!
   lectureList.innerHTML = ""
   let displayEvents = getDisplayCalendarEvents()
   lectureList.insertAdjacentHTML("beforeend", calendarTemplate({items:displayEvents}))
   document.querySelectorAll(".lecture-entry-listitem").forEach(el => {
-    el.addEventListener("click", (e) => handleLectureEntryClick(e))
+    el.addEventListener("click", async (e) => await handleLectureEntryClick(e))
   })
 }
 
-function handleRenameModalConfirm() {
+async function handleRenameModalConfirm() {
   let newNameInput = document.querySelector<HTMLInputElement>("#new-name-modal-input")!
-  console.log(newNameInput.value)
+  
   if (newNameInput.value == "") {
     alert("Events cannot have an empty name.")
     return
   }
   
+  let modal = document.querySelector("#renameModal")!
+  bootstrap.Modal.getInstance(modal).toggle()
+  document.querySelector('.modal-backdrop').remove();
+  app.modalOpen = false
+
   let checkedIds = getCheckedIds()
   checkedIds.map(checkedId => {
     Array.from(app.event_map.entries()).filter(event => event[1].id == checkedId)
       .map(event => event[1].final_name = newNameInput.value)
   })
-  populateCalendarEntries()
-  console.log()
+  await populateCalendarEntries()
+  
 }
 
-function handleLoadedCalendarFile(file_text: string) {
-  document.querySelector("#calendar-input-container")!.outerHTML = ""
+async function waitForAnimationEnd(...args: HTMLElement[]): Promise<Promise<any>[]> {
+  return args.map(el => {
+    return new Promise(resolve => {
+      el.addEventListener('animationend', resolve, { once: true });
+    });
+  })
+}
+
+async function handleLoadedCalendarFile(file_text: string) {
+  let calendarInputContainer = document.querySelector<HTMLInputElement>("#calendar-input-container")!
+  let instructionsLoadCalendar = document.querySelector<HTMLDivElement>("#instructions-load-calendar")!
+  let firstBlockAnimationEnd = waitForAnimationEnd(calendarInputContainer, instructionsLoadCalendar)
+  instructionsLoadCalendar.style.animationPlayState = "running"
+  calendarInputContainer.style.animationPlayState = "running"
+  await Promise.all(await firstBlockAnimationEnd)
+  instructionsLoadCalendar.remove()
+  calendarInputContainer.remove()
   
+  let instructionsEditCalendar = document.querySelector<HTMLInputElement>("#instructions-edit-calendar")!
+  let secondBlockAnimationInEnd = waitForAnimationEnd(instructionsEditCalendar)
+  instructionsEditCalendar.classList.remove("d-none")
+  instructionsEditCalendar.style.animationPlayState = "running"
+  await secondBlockAnimationInEnd
+
+  await new Promise(resolve => setTimeout(resolve, 500));
+
   let calendar = ICAL.parse(file_text)
   app.ical = calendar
   let component = new ICAL.Component(calendar)
